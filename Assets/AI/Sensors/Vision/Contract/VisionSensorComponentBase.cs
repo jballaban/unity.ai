838b5b1f468df35d6d3068424d664fa102a7948f
James Ballaban <jballaban@gmail.com>
2018-09-23 22:07:16 -0400

    Doing some basic vision tests

--- Assets/AI/Sensors/Vision/Contract/VisionSensorComponentBase.cs
+++ Assets/AI/Sensors/Vision/Contract/VisionSensorComponentBase.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public abstract class VisionSensorComponentBase : MonoBehaviour
 {
+    SphereCollider _collider;
+    void Start()
+    {
+        _collider = GetComponent<SphereCollider>();
+    }
 
     public abstract void Lose(GameObject other);
 
     public abstract void Perceive(GameObject other);
 
-    public virtual bool IsVisible(GameObject target) { return true; }
+    public virtual bool IsVisible(GameObject target)
+    {
+        var dir = target.transform.position - transform.position;
+        RaycastHit result;
+        var wasHit = Physics.Raycast(transform.position, dir, out result, _collider.radius);
+        Debug.DrawRay(transform.position, dir, Color.red, 1f);
+        return wasHit;
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.isTrigger) return;
-        Debug.Log(transform.parent.name + " Sees " + other.transform.name);
+        Debug.Log(transform.parent.name + " collides with " + (other.transform.parent ?? other.transform).name);
         if (IsVisible(other.gameObject))
+        {
+            Debug.Log(transform.parent.name + " sees " + (other.transform.parent ?? other.transform).name);
             Perceive(other.gameObject);
+        }
     }
 
     void OnTriggerExit(Collider other)
